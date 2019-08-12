f45639eb77c60c3456b24daf7b9e94e673f99cc7
lnrdlngn <51335899+lnrdlngn@users.noreply.github.com>
2019-08-12 12:54:15 +0200

    app

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+// Force development builds
+workbox.setConfig({ debug: true });
+
+
+
+// Cache all files
+workbox.routing.registerRoute(
+    new RegExp('/'),
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
+// Cache JS files
+workbox.routing.registerRoute(
+    new RegExp('.+\\.js$'),
+    new workbox.strategies.StaleWhileRevalidate()
+);
+
+// Cache CSS files
+workbox.routing.registerRoute(
+    new RegExp('.+\\.css$'),
+    new workbox.strategies.StaleWhileRevalidate()
+);
